838bf05f9a2ef3da46f7a77ea899475eb7324082
ginoongflores <flores.135577@cdo.sti.edu.ph>
2022-02-26 14:21:15 +0800

    Implementing Time Travel

--- src/index.js
+++ src/index.js
@@ -82,7 +82,7 @@ class Game extends React.Component {
     const moves = history.map((step, move) => {
       const desc = move ? "Go to move #" + move : "Go to game start";
       return (
-        <li>
+        <li key={move}>
           <button onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
