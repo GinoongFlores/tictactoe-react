dce41d3ea52b922ea68d21242b72177c848680d1
ginoongflores <flores.135577@cdo.sti.edu.ph>
2022-02-28 19:50:08 +0800

    Revert "Wrapping Up!"
    This reverts commit 6e37991276d344d93fcf1ba792147bca35f4f287.

--- src/index.js
+++ src/index.js
@@ -52,13 +52,12 @@ class Game extends React.Component {
           squares: Array(9).fill(null),
         },
       ],
-      stepNumber: 0,
       xIsNext: true, //a boolean value
     };
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const history = this.state.history;
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if (calculateWinner(squares) || squares[i]) {
@@ -71,22 +70,13 @@ class Game extends React.Component {
           squares: squares,
         },
       ]),
-      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
-    });
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0,
     });
   }
 
   render() {
     const history = this.state.history;
-    // const current = history[history.length - 1];
-    const current = history[this.state.stepNumber];
+    const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
